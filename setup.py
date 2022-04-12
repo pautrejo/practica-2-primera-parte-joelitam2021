86761c2e3ce064c5a320c02159e3f2dd23ff9eed
Aide Jazmín González Cruz <aide.jgc@gmail.com>
2022-04-11 21:51:53 -0500

    Agregando setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bellman_ford",
+    version="0.0.1",
+    author="Equipo",
+    description="Método numérico que resuelva problemas de optimización convexa de pequeña escala.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/optimizacion-2-2022-gh-classroom/practica-1-segunda-parte-pautrejo",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        'numpy>=1.19.2'
+        ],
+    python_requires=">=3.7.12",
+)
