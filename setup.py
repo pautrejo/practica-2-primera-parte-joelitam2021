fc42b2025fc578bd7158dcc7ef7ca6cfdcb54637
joelitam2021 <joel.jaramillo@itam.mx>
2022-04-16 13:48:36 -0500

    cambio en el URL a practica 2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     description="Método numérico que resuelva problemas de optimización convexa de pequeña escala.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/optimizacion-2-2022-gh-classroom/practica-1-segunda-parte-pautrejo",
+    url="https://github.com/optimizacion-2-2022-gh-classroom/practica-2-primera-parte-joelitam2021",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     install_requires=[
