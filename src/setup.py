594f7fa89af6dec46e5f97496baad10bdebfc3b2
pautrejo <42397583+pautrejo@users.noreply.github.com>
2022-04-22 15:06:36 -0500

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,15 +1,14 @@
 #see: https://setuptools.readthedocs.io/en/latest/userguide/package_discovery.html
 from setuptools import setup, find_packages
 
-setup(name="opt2",
-      version="0.1",
+setup(name="bellman_ford",
+      version="0.0.1",
       description=u"Small package for example",
       url="",
-      author="itam",
-      author_email="",
-      license="MIT",
+      author="Equipo",
       packages=find_packages(),
       install_requires = [
-                          "numpy",
+                          "numpy>=1.19.2"
                           ],
+       python_requires=">=3.7.12",
       )
