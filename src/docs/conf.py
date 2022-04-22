73442d6f9d16fa41ad517a7121395cb31abc6ead
pautrejo <42397583+pautrejo@users.noreply.github.com>
2022-04-21 19:04:18 -0500

    cambio version

--- src/docs/conf.py
+++ src/docs/conf.py
@@ -22,7 +22,7 @@ author = 'Paulina, Aide, Joel, Enrique'
 master_doc = 'index'
 add_module_names = False
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
